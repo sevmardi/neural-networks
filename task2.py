c561b245d44324ff871e4d135103bdd0d6a1b721
Sevak Mardirosian <smardirosians@gmail.com>
2018-02-28 13:00:37 +0100

    2version of task2

---
+++ task2.py
@@ -0,0 +1,105 @@
+import numpy as np
+from collections import OrderedDict
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import itertools
+import pandas as pd
+
+train_in = np.genfromtxt("data/train_in.csv", delimiter=",")
+test_in = np.genfromtxt("data/test_in.csv", delimiter=",")
+# label values
+train_out = np.genfromtxt("data/train_out.csv")
+test_out = np.genfromtxt("data/test_out.csv")
+
+
+# train_in = pd.read_csv('data/train_in.csv', sep=',',header=None)
+# train_out = pd.read_csv('data/train_out.csv', sep=',',header=None)
+# test_in = pd.read_csv('data/test_in.csv', sep=',',header=None)
+# test_out = pd.read_csv('data/test_out.csv', sep=',',header=None)
+
+
+# Prepare the data
+# train = pd.concat([train_out, train_in], axis = 1)
+# train.columns = range(0, 257)
+
+# test = pd.concat([test_out, test_in], axis = 1)
+# test.columns = range(0, 257)
+
+
+# create ordered dictionary with (list_#digit: vectors)
+d = OrderedDict(("list_" + str(i), []) for i in range(10))
+
+for i in range(10):
+	for j in range(len(train_out)):
+		if train_out[j] == i:
+			d["list_" + str(i)].append(train_in[j, :])
+
+# means/centers as (10,256)-array 
+centers = np.zeros((10, 256)) 
+# centers = train.groupby(train_out[0]).mean() 
+
+for i in range(10):
+	centers[i, :] = np.mean(d["list_" + str(i)], axis=0)
+
+for i in range(10):
+	print("number of " + str(i) + "s: " + str(len(d["list_" + str(i)])))
+
+# calculate radii
+radii = np.zeros((10, 1))
+for i in range(10):
+	radius = 0
+	for point in d["list_" + str(i)]:
+		new_radius = np.linalg.norm(point-centers[i,:])
+		if new_radius >= radius:
+			radius = new_radius
+	radii[i] = radius
+
+
+#create distance matrix between centers
+centers_dist = np.zeros((10,10))
+
+for i in range(10):
+	for j in range(10):
+		centers_dist[i,j] = np.linalg.norm(centers[i, :] - centers[j, :])
+
+# print("Distances between each centers")
+# print(centers_dist)
+
+train_pre = np.empty(len(train_out))
+test_pre = np.empty(len(test_out))
+
+for i in range(len(train_in)):
+	current_dist = pairwise_distances(centers, train_in[i], metric='cosine')
+	train_pre[i] = np.argmin(current_dist)
+
+confusion_matrix_test = confusion_matrix(test_out, test_pre)
+correct_rate_train = np.zeros(10)
+
+
+
+conf_matrix_train = confusion_matrix(train_out, train_pre)
+
+
+# # calculate the correctly classified digits
+correct_rate_train = np.zeros(10)
+for i in range(10):
+    correct_rate_train[i] = float(
+        conf_matrix_train[i, i]) / np.sum(conf_matrix_train[i, :])
+print("Correct rate of training")
+print(correct_rate_train)
+
+# create confusion matrix for test data
+for i in range(len(test_out)):
+    current_dist = pairwise_distances(centers, test_in[i], metric='cosine')
+    test_pre[i] = np.argmin(current_dist)
+
+conf_matrix_test = confusion_matrix(test_out, test_pre)
+
+correct_rate_test = np.zeros(10)
+for i in range(10):
+    correct_rate_test[i] = float(
+        conf_matrix_test[i, i]) / np.sum(conf_matrix_test[i, :])
+print("correct rate on testing data")
+
+# print(correct_rate_test)
---
+++ task_01_02.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.spatial.distance import pdist
+from scipy.spatial.distance import squareform
+import sklearn.metrics.pairwise
+
+
+train_in = pd.read_csv('data/train_in.csv', sep=',', header=None)
+train_out = pd.read_csv('data/train_out.csv', sep=',', header=None)
+test_in = pd.read_csv('data/test_in.csv', sep=',', header=None)
+test_out = pd.read_csv('data/test_out.csv', sep=',', header=None)
+
+train_in.head()
+train_out.head()
+test_in.head()
+test_out.head()
+
+
+# Prepare the data
+train = pd.concat([train_out, train_in], axis=1)
+train.columns = range(0, 257)
+
+test = pd.concat([test_out, test_in], axis=1)
+test.columns = range(0, 257)
+
+# Task1
+ts1 = train.groupby(train_out[0])
+
+# Calculate the centroid of the 10 classes.
+centroids = train.groupby(train_out[0]).mean()
+
+radius = [[]] * 10
+for i in range(0, 10):
+    squared = (train_in[train_out[0] == i] - centroids.iloc[i, :])**2
+    radius[i] = np.sum(squared, axis=1).max()**(0.5)
+
+dist_cen = pdist(centroids, 'euclidean')
+dis_mat = squareform(dist_cen)
+
+# print(dis_mat)
+
+out_label = []
+for i in range(0, 1707):
+    squared = (train_in.loc[i, :] - centroids)**2
+    dist = np.sum(squared, axis=1) ** (0.5)
+    out_label.append(np.argmin(dist))
+
+# print(out_label)
+out_label_test = []
+for i in range(0, 1000):
+    squared = (train_in.loc[i, :] - centroids)**2
+    dist = np.sum(squared, axis=1) ** (0.5)
+    out_label_test.append(np.argmin(dist))
+
+print(out_label_test)
