f4bb75c022e2a6fafa5229c7dcbd39f8c5eb4ff0
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2018-03-09 18:33:40 +0100

    updated bayes classifcation

--- steinar/a1-3.py
+++ steinar/a1-3.py
@@ -27,11 +27,11 @@ print(subimage.shape)
 
 #plotting integers and their lower halves
 im = plt.imshow(image, cmap='gray')
-plt.savefig('report/figures/integers/'+str(itemid)+'.png')
+#plt.savefig('report/figures/integers/'+str(itemid)+'.png')
 plt.show()
 
 im2 = plt.imshow(subimage, cmap='gray')
-plt.savefig('report/figures/integers/lh-'+str(itemid)+'.png')
+#plt.savefig('report/figures/integers/lh-'+str(itemid)+'.png')
 plt.show()
 
 
@@ -107,26 +107,36 @@ plt.legend(handles=[fives,sevens])
 plt.xlabel('intensity with 2 decimal space precision')
 plt.ylabel('occurrence')
 plt.title('Histogram of bottom half mean intensities for 5s and 7s')
-plt.savefig('report/figures/plots/57hist.png')
+#plt.savefig('report/figures/plots/57hist.png')
 plt.show()
 
 
-#%% bayes classification P(C|X)
+#%% bayes classification P(C_k|X)
 
 values_57 = [lh_intensity[key] for key in lh_intensity]
 mean_57 = float(format( statistics.mean(values_57), '.2f' ))
+P_C5 = sum(hist5.values()) / (sum(hist7.values()) + sum(hist5.values()))
+P_C7 = sum(hist7.values()) / (sum(hist7.values()) + sum(hist5.values()))
 
 for key, value in lh_intensity.items():
     correct_57[key] = int(train_out[key])
+    #in case of uncertainty, if the value could either represent a 5 or a 7, apply bayes theorem
     if(value in hist5.keys() and value in hist7.keys() ):
-        if(np.random.rand() > hist5[value] / hist7[value]):
+        P_X_C5 = hist5[value] / (hist7[value] + hist5[value])
+        P_X_C7 = hist7[value] / (hist7[value] + hist5[value])
+        P_X = P_X_C5 * P_C5 + P_X_C7 * P_C7
+        P_C5_X = (P_X_C5 * P_C5) / P_X
+        P_C7_X = (P_X_C7 * P_C7) / P_X
+        if(np.random.rand() > P_C5_X):
             classify_57[key] = 7
         else:
             classify_57[key] = 5
+    #fallback to classifying by mean.
     elif value < mean_57:
         classify_57[key] = 7
     else:
         classify_57[key] = 5
+
 
 print(correct_57)
 print(classify_57)
@@ -135,6 +145,4 @@ a = np.array(list(correct_57.values()))
 b = np.array(list(classify_57.values()))
 
 print(confusion_matrix(list(correct_57.values()), list(classify_57.values())))
-
-
 (a == b).sum() / len(a)
