725e3dca2dbd9c0c0e48513a641061a659973a0c
Sevak Mardirosian <smardirosians@gmail.com>
2018-04-12 10:42:45 +0200

    forgot to uncomment the autoencoder

--- assignment2/task3/autoencoder.py
+++ assignment2/task3/autoencoder.py
@@ -50,8 +50,8 @@ x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 # print(x_train.shape)
 # print(x_test.shape)
 
-# autoencoder.fit(x_train, x_train, epochs=50, batch_size=256,
-#                 shuffle=True, validation_data=(x_test, x_test))
+autoencoder.fit(x_train, x_train, epochs=50, batch_size=256,
+                shuffle=True, validation_data=(x_test, x_test))
 
 encoded_imgs = encoder.predict(x_test)
 decoded_imgs = decoder.predict(encoded_imgs)
