d4532ff0251c23bf0fc1a70fc19bee28c9caf10c
Sevak Mardirosian <smardirosians@gmail.com>
2018-04-12 10:30:52 +0200

    fix

--- assignment2/task3/autoencoder.py
+++ assignment2/task3/autoencoder.py
@@ -1,5 +1,7 @@
 from keras.layers import Input, Dense
 from keras.models import Model
+import pickle 
+
 # import resource
 # resource.setrlimit(resource.RLIMIT_NPROC, (1, 1))
 
@@ -27,7 +29,6 @@ encoded_input = Input(shape=(encoding_dim,))
 # retrieve the last layer of the autoencoder model
 decoder_layer = autoencoder.layers[-1]
 
-
 # create the decoder model
 decoder = Model(encoded_input, decoder_layer(encoded_input))
 
@@ -46,8 +47,8 @@ x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-print(x_train.shape)
-print(x_test.shape)
+# print(x_train.shape)
+# print(x_test.shape)
 
 autoencoder.fit(x_train, x_train, epochs=50, batch_size=256,
                 shuffle=True, validation_data=(x_test, x_test))
@@ -55,9 +56,10 @@ autoencoder.fit(x_train, x_train, epochs=50, batch_size=256,
 encoded_imgs = encoder.predict(x_test)
 decoded_imgs = decoder.predict(encoded_imgs)
 
+pickle.dump(decoded_imgs, open("decoded_imgs_50runs.pickle", "wb" ))
+
 
 import matplotlib.pyplot as plt
-
 n = 10  # digits to display
 plt.figure(figsize=(20, 4))
 for i in range(n):
@@ -74,4 +76,6 @@ for i in range(n):
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
-plt.savefig('plot.png')
+
+
+# plt.savefig('plot.png')
