24fe0012f38301576e8638f39ce333e12b356ffd
Sevak Mardirosian <smardirosians@gmail.com>
2018-04-15 12:03:32 +0200

    pickles added.

--- assignment2/task3/convolutional_autoencoder.py
+++ assignment2/task3/convolutional_autoencoder.py
@@ -1,6 +1,7 @@
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
 from keras import backend as K
+import pickle as pic
 
 input_img = Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
 
@@ -38,8 +39,33 @@ x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `ch
 
 from keras.callbacks import TensorBoard
 autoencoder.fit(x_train, x_train,
-                epochs=3,
+                epochs=50,
                 batch_size=128,
                 shuffle=True,
                 validation_data=(x_test, x_test),
                 callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+
+
+decoded_imgs = autoencoder.predict(x_test)
+
+pic.dump(decoded_imgs, open("pickles/convolutional_autoencoder_50.pickle", "wb"))
+
+# import matplotlib.pyplot as plt
+
+# n = 10
+# plt.figure(figsize=(20, 4))
+# for i in range(n):
+# 	# display original
+# 	ax = plt.subplot(2, n, i)
+# 	plt.imshow(x_test[i].reshape(28, 28))
+# 	plt.gray()
+# 	ax.get_xaxis().set_visible(False)
+# 	ax.get_yaxis().set_visible(False)
+
+# 	ax = plt.subplot(2, n, i + n)
+# 	plt.imshow(decoded_imgs[i].reshape(28, 28))
+# 	plt.gray()
+# 	ax.get_xaxis().set_visible(False)
+# 	ax.get_yaxis().set_visible(False)
+
+# plt.savefig('plots/50_convolutional_autoencoder.png')
