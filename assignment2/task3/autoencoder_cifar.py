d331f15bac7de457c02860790d4e1621702f6391
Sevak Mardirosian <smardirosians@gmail.com>
2018-04-16 13:46:15 +0200

    plt error fix-try

--- assignment2/task3/autoencoder_cifar.py
+++ assignment2/task3/autoencoder_cifar.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     n = 10
     plt.figure(figsize=(20, 4))
     for i in range(n):
-    	ax = plt.subplot(2, n, i)
+    	ax = plt.subplot(2, n, i + 1)
     	plt.imshow(x_test[i].reshape(28, 28))
     	plt.gray()
     	ax.get_xaxis().set_visible(False)
